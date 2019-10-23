4db8f622d125f2ff5995ae297d680a4b0257b1ad
Flow <florian.gustin.barriera@gmail.com>
2019-10-24 01:28:29 +0200

    je dois decoder json object mais je recup bien mes data

---
+++ where/where/model/manager/ApiService.swift
@@ -0,0 +1,9 @@
+//
+//  ApiService.swift
+//  where
+//
+//  Created by FLORIAN GUSTIN on 23/10/2019.
+//  Copyright © 2019 Elite. All rights reserved.
+//
+
+import Foundation
---
+++ where/where/model/manager/CustomApiService.swift
@@ -0,0 +1,9 @@
+//
+//  CustomApiService.swift
+//  where
+//
+//  Created by FLORIAN GUSTIN on 24/10/2019.
+//  Copyright © 2019 Elite. All rights reserved.
+//
+
+import Foundation
