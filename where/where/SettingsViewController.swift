cb14be0075651830a1436a61a0b61418265910c9
Cédric <cedric.leprohon@yahoo.fr>
2019-10-24 16:22:29 +0200

    on va merge

--- where/where/SettingsViewController.swift
+++ where/where/SettingsViewController.swift
@@ -12,31 +12,12 @@ class SettingsViewController: UIViewController {
 
     @IBOutlet weak var btnMap: UISegmentedControl!
     @IBOutlet weak var btnAction: UIButton!
-    @IBOutlet weak var btnLanguage: UIButton!
-    @IBOutlet weak var btnDepart: UIButton!
-    @IBOutlet weak var switchAutoDark: UISwitch!
-    @IBOutlet weak var switchManualDark: UISwitch!
+    @IBOutlet weak var darkMode: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if SettingsController.isTrue(key: "autoDarkMode") {
-            switchManualDark.isEnabled = false;
-        }
-        
-        switchAutoDark.isOn = SettingsController.isTrue(key: "autoDarkMode");
-        switchManualDark.isOn = SettingsController.isTrue(key: "darkMode");
-        
-        var langue = "";
-        
-        switch SettingsController.get(key: "langue")
-        {
-        case "en":
-            langue = "English"
-        case "fr":
-            langue = "Français"
-        default:
-            langue = "Walof"
-        }
+      
         
         switch SettingsController.get(key: "map")
         {
@@ -48,47 +29,33 @@ class SettingsViewController: UIViewController {
             btnMap.selectedSegmentIndex = 0;
         }
         
-        
-        changeLabelText(btn: btnLanguage, text: langue)
-        
+     
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func switchManualDarkPressed(_ sender: Any) {
-        if switchManualDark.isOn {
-            overrideUserInterfaceStyle = .dark
-            SettingsController.set(val: true, key: "darkMode")
-        }else {
-            overrideUserInterfaceStyle = .light
-            SettingsController.set(val: false, key: "darkMode")
-        }
-        
-    }
     
-    @IBAction func switchAutoDarkPressed(_ sender: Any) {
-        var auto : Bool = false;
-        if !switchAutoDark.isOn {
-            
+    
+    @IBAction func darkModeChange(_ sender: Any) {
+        if(darkMode.selectedSegmentIndex == 0) {
+            SettingsController.set(val: true, key: "autoDarkMode")
             SettingsController.set(val: false, key: "darkMode")
-            switchManualDark.isEnabled = true;
+            switch traitCollection.userInterfaceStyle {
+                case .light, .unspecified:
+                overrideUserInterfaceStyle = .light
+                case .dark:
+                overrideUserInterfaceStyle = .dark
 
+            }
+        }else if(darkMode.selectedSegmentIndex == 1) {
+            SettingsController.set(val: false, key: "autoDarkMode")
+            SettingsController.set(val: true, key: "darkMode")
+            overrideUserInterfaceStyle = .dark
 
-        }
-        else {
-            auto = true;
-            switchManualDark.isEnabled = false;
-
-            switchManualDark.setOn(false, animated: true)
+        }else {
+            SettingsController.set(val: true, key: "autoDarkMode")
+            SettingsController.set(val: false, key: "darkMode")
             overrideUserInterfaceStyle = .light
-            SettingsController.set(val: false, key: "darkMode")
 
         }
-        print(auto);
-
-        SettingsController.set(val: auto, key: "autoDarkMode")
-        
-
-        
     }
     
     @IBAction func btnMapPressed(_ sender: Any) {
@@ -109,42 +76,7 @@ class SettingsViewController: UIViewController {
         SettingsController.set(val: map, key: "map");
         print(map);
     }
-    @IBAction func btnChangeLanguage(_ sender: Any) {
-        var langue : String = SettingsController.get(key: "langue");
-        let otherAlert = UIAlertController(title: SettingsController.get(key: "langue"), message: "Il se pourrait que vous devriez redémarrer l'application.", preferredStyle: UIAlertController.Style.actionSheet)
-
-        let fr = UIAlertAction(title: "Français", style: UIAlertAction.Style.default) { _ in
-            self.changeLabelText(btn: self.btnLanguage, text: "Français")
-            langue = "fr";
-            SettingsController.set(val: langue, key: "langue");
-
-            
-        }
-        
-        let en = UIAlertAction(title: "English", style: UIAlertAction.Style.default) { _ in
-            self.changeLabelText(btn: self.btnLanguage, text: "English")
-            langue = "en";
-            SettingsController.set(val: langue, key: "langue");
-
-
-
-        }
-        
-        let wf = UIAlertAction(title: "Walof", style: UIAlertAction.Style.default) { _ in
-            self.changeLabelText(btn: self.btnLanguage, text: "Walof")
-            langue = "wf";
-            
-
-        }
-
-        // relate actions to controllers
-        otherAlert.addAction(en)
-        otherAlert.addAction(fr)
-        otherAlert.addAction(wf)
-        
-        
-        present(otherAlert, animated: true, completion: nil)
-    }
+    
     
     func changeLabelText(btn : UIButton, text : String) {
         btn.setTitle(text, for: .normal)
@@ -162,4 +94,5 @@ class SettingsViewController: UIViewController {
             })
         }
     }
+
 }
