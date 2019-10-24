3e605c9a9a552209a40542a351ac912868003ebd
Flow <florian.gustin.barriera@gmail.com>
2019-10-24 16:27:36 +0200

    je merge

--- where/where/model/manager/CustomApiService.swift
+++ where/where/model/manager/CustomApiService.swift
@@ -12,22 +12,17 @@ import SwiftyJSON
 
 class CustomApiService {
     
-    static func getData(string: String, completion: @escaping (_ response: Any) -> Void) {
+    static func getData(startCoord: String, endCoord: String, completion: @escaping (_ response: Any) -> Void) {
         let session = URLSession.shared
-        let link: String = "https://developer.citymapper.com/api/1/traveltime/?"
-        let startCoord: String = "48.886009%2C2.289127"
-        let endCoord: String = "48.896325%2C2.291561"
-        let time: String = "2014-11-06T19%3A00%3A02-0500"
-        let timeType: String = "arrival"
-        let key: String = "6c69c29aee0df15279aac830461a2976"
         
-        let url = URL(string: "https://developer.citymapper.com/api/1/traveltime/?startcoord=48.886009%2C2.289127&endcoord=48.896325%2C2.291561&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=6c69c29aee0df15279aac830461a2976")!
+           let key: String = "5e83a030bf0f924bf2c8c0c5adccf866"
 
-        let urlBis = URL(string: link + "startcoord=" + startCoord + "&endcoord=" + endCoord + "&time=" + time + "&time_type=" + timeType + "&key=" + key )!
-        print(url)
+        let urlBis = URL(string: "https://developer.citymapper.com/api/1/traveltime/?startcoord=" + startCoord + "&endcoord=" + endCoord + "&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=" + key )!
+        
+        print(urlBis)
         
 
-        let task = session.dataTask(with: url) { data, response, error in
+        let task = session.dataTask(with: urlBis) { data, response, error in
 
             if error != nil || data == nil {
                 print("Client error!")
@@ -51,11 +46,8 @@ class CustomApiService {
                 let tryTmp = json["travel_time_minutes"]
                 
                 completion(tryTmp! as Any)
-                print(tryTmp!)
                 }
                 
-                
-
             }catch {
                 print("JSON error: (error.localizedDescription)")
             }
