5517746dc836e8df805cb91d09b706f836141ee8
Unknown <boubacar.yatera@edu.itescia.fr>
2019-10-24 14:38:08 +0200

    My firt psuh

--- where/where/FirstViewController.swift
+++ where/where/FirstViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var lblTitle : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblTitle.text = NSLocalizedString("title_text", comment: "")
     }
 
 
